58f82fa4e016769da223688469328180cacf656d
Alexander Granhof <alexander.g737@gmail.com>
2020-11-14 17:57:44 +0100

    install dev deps twice since the coverage module cannot be found

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 . .venv/bin/activate
 
 # run this if the module coverage is not found
-# python3 -m venv .venv
-# pip3 install -r requirements/dev.txt
+python3 -m venv .venv
+pip3 install -r requirements/dev.txt
 
 python3 -m coverage run --rcfile=.coveragerc -m py.test -c pytest.ini tests/unit
