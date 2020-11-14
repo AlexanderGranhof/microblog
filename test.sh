31e3008a81813d282ce61018d995637029bb3f3e
Alexander Granhof <alexander.g737@gmail.com>
2020-11-14 16:14:44 +0100

    created a test script for docker

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+. .venv/bin/activate
+
+# run this if the module coverage is not found
+python3 -m venv .venv
+pip3 install -r requirements/dev.txt
+
+python3 -m coverage run --rcfile=.coveragerc -m py.test -c pytest.ini tests/unit
